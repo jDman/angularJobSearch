c2871fc819b2045e351dc867a2fce74101828638
Jay Davis <davis.jaym@gmail.com>
2015-04-29 15:51:18 +0100

    Updated filters

--- src/app/directives/search.js
+++ src/app/directives/search.js
@@ -11,8 +11,28 @@ angular.module('jobSearch')
             searchBtn = $('#searchBtn'),
             selectedFilter = $('#searchFilter');
 
+        input.on('keypress', function(evt){
+          var key;
+
+          if(evt.key){
+            key = evt.key;
+          } else {
+            key = evt.keyCode;
+          }
+
+          if(key === 13 || key === 'Enter') {
+            if(this.value !== ''){
+              scope.jobSearch(this.value);
+            }
+          }
+
+          return;
+        });
+
         searchBtn.on('click', function(evt){
-          scope.jobSearch(input.val());
+          if(input.val() !== ''){
+            scope.jobSearch(input.val());            
+          }
         });
 
         selectedFilter.on('change', function(){
